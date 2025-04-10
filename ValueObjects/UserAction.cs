1948322af132d7d2e8ebc5c0e7dbe50eea9b7d5a
Juan Gomez <juangomez130703@gmail.com>
2025-04-10 00:08:28 -0500

    Console_App

--- ValueObjects/UserAction.cs
+++ ValueObjects/UserAction.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace NorthWindEntities.ValueObjects
 {
-    public class UserAction
+    public class UserAction(string user, string description)
     {
+        public DateTime CreatedDateTime { get; } = DateTime.Now;
+        public string User => user;
+        public string Description => description;
     }
 }

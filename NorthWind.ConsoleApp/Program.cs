b38ec624e45dd70f2e9604bae3e5cb805246c151
Juan Gomez <juangomez130703@gmail.com>
2025-04-10 23:59:07 -0500

    NorthWindIoC

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -7,9 +7,12 @@ using Microsoft.Extensions.DependecyInjection;
 
 
 HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
-Builder.Services.AddConsoleWriter();
+
+Builder.Services.AddNorthWindServices();
+
 Builder.Services.AddSingleton<AppLogger>();
 Builder.Services.AddSingleton<ProductService>();
+
 using IHost AppHost = Builder.Build();
 
 AppLogger Logger = AppHost.Services.GetRequiredService<AppLogger>();
---
+++ NorthWind.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Writers;
+using NorthWindEntities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependecyInjection;
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddNorthWindServices(this IServiceCollection services)
+            { 
+                services.AddDebugWriter();
+                return services;
+            }
+}
